2583442b9f9ac44c440bf4b1acb187a8239dab2c
HectorEsteve <hectorestevex2@gmail.com>
2024-05-13 10:52:05 +0200

    cambios en factory de vinilos

--- database/factories/VinylFactory.php
+++ database/factories/VinylFactory.php
@@ -6,14 +6,15 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class VinylFactory extends Factory{
     public function definition(): array
-    {
-        return [
-            'name' => $this->faker->sentence(3),
-            'cover_url' => $this->faker->optional()->imageUrl(),
-            'publication_year' => $this->faker->numberBetween(1970, 2022),
-            'edition_year' => $this->faker->numberBetween(19700, 2022),
-            'format_id' => $this->faker->numberBetween(1, 8), // Assuming there are 5 format types
-            'record_company_id' => $this->faker->numberBetween(1, 3), // Assuming there are 10 record companies
-        ];
-    }
+{
+    $publicationYear = $this->faker->numberBetween(1970, 2022);
+    return [
+        'name' => $this->faker->sentence(3),
+        'cover_url' => $this->faker->optional()->imageUrl(),
+        'publication_year' => $publicationYear,
+        'edition_year' => $this->faker->numberBetween($publicationYear, 2022), // Asegura que sea mayor o igual al año de publicación
+        'format_id' => $this->faker->numberBetween(1, 8), // Suponiendo que hay 5 tipos de formato
+        'record_company_id' => $this->faker->numberBetween(1, 3), // Suponiendo que hay 10 compañías discográficas
+    ];
+}
 }
